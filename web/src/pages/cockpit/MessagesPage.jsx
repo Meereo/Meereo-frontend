8eaad8f4d8f643c66d884c0e8c5ed6be43af4299
root <root@srv1253134.hstgr.cloud>
2026-06-19 15:51:29 +0000

    fix: remove JSX comment after ternary closing div in MessagesPage (syntax error)

--- web/src/pages/cockpit/MessagesPage.jsx
+++ web/src/pages/cockpit/MessagesPage.jsx
@@ -1059,7 +1059,7 @@ export default function MessagesPage({ showToast }) {
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><line x1="22" y1="2" x2="11" y2="13" /><polygon points="22 2 15 22 11 13 2 9 22 2" /></svg>
                   </button>
                   </div>{/* inner input row */}
-                </div>{/* outer input+banner wrapper */}
+                </div>
               )}
             </>
           )}
